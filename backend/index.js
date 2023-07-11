0399f631c49af42907210a551077e9cd76fa8664
JaehyeonPaak <wogus392@gmail.com>
2023-07-12 00:03:52 +0900

    feat: creating pins router with save and post method

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const app = express();
 // .env 파일을 불러옴
 dotenv.config();
 
+// app.use() 함수는 Express 앱에서 항상 실행하는 미들웨어의 역할, URL에 상관없이 앱이 요청을 수신할 때마다 매번 실행
+// It parses incoming JSON requests and puts the parsed data in req.body
+app.use(express.json());
+
 // Mongo DB 연결
 mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
---
+++ backend/routes/pins.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const Pin = require('../models/Pin');
+
+// create a pin
+router.post('/', async (req, res) => {
+    const newPin = new Pin(req.body);
+    try {
+        // async와 await 사용하지 않을 시 저장되는 시점 전에 반환하는 savedPin 변수가 undefined일 수도 있기 때문에 선언 
+        const savedPin = await newPin.save();
+        res.status(200).json(savedPin);
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// get all pins
+
+module.exports = router;
