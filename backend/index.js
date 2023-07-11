aca84cd9107f0643b90eaaac11780fd0efdbee43
JaehyeonPaak <wogus392@gmail.com>
2023-07-11 22:39:36 +0900

    feat: connecting to mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 
+// .env 파일을 불러옴
+dotenv.config();
+
+// Mongo DB 연결
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Mongo DB connected!');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+// 포트 번호 1035로 연결
 app.listen(1035, () => {
     console.log('Backend server is running!');
-});
+    });
