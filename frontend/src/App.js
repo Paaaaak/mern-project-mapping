b3e6baf3e530aece7fd718199a8559f261e2f5f8
JaehyeonPaak <wogus392@gmail.com>
2023-07-15 15:04:46 +0900

    style: change map style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,14 +16,12 @@ function App() {
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         initialViewState={viewState}
         style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      >
+        mapStyle="mapbox://styles/jaehyeonpaak/clk3lonwv000q01rd0jcu3lsf">
         <Marker 
           longitude={-122.4}
           latitude={37.8}
-          anchor='bottom'
-        >
-          <Room></Room>
+          anchor='bottom'>
+          <Room style={{fontSize: viewState.zoom * 6, color: 'red'}}></Room>
         </Marker>
       </Map>
     </div>
