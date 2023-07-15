aad1b4011dd1c20a113da43d1d5b8ec66d754ae4
JaehyeonPaak <wogus392@gmail.com>
2023-07-15 23:04:31 +0900

    feat: create pop up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
-import Map, {Marker} from 'react-map-gl';
-
-import {Room} from '@material-ui/icons'
+import Map, {Marker, Popup} from 'react-map-gl';
+import {Room, Star} from '@material-ui/icons'
+import './App.css';
 
 function App() {
   const [viewState, setViewState] = useState({
@@ -9,6 +9,8 @@ function App() {
     latitude: 37.8,
     zoom: 8
   });
+
+  const [showPopup, setShowPopup] = useState(true);
 
   return (
     <div className='App'>
@@ -23,6 +25,29 @@ function App() {
           anchor='bottom'>
           <Room style={{fontSize: viewState.zoom * 6, color: 'red'}}></Room>
         </Marker>
+        {/* <Popup 
+          longitude={-122.4} 
+          latitude={37.8}
+          anchor="left"
+          onClose={() => setShowPopup(false)}>
+          <div className='card'>
+            <label>Place</label>
+            <h4 className='place'>Lotte Tower</h4>
+            <label>Review</label>
+            <p className='description'>Fascinating</p>
+            <label>Rating</label>
+            <div className='stars'>
+              <Star className='star'></Star>
+              <Star className='star'></Star>
+              <Star className='star'></Star>
+              <Star className='star'></Star>
+              <Star className='star'></Star>
+            </div>
+            <label>Information</label>
+            <span className='username'>Created by <b>jaehyeonpaak</b></span>
+            <span className='date'>1 hour ago</span>
+          </div>
+        </Popup> */}
       </Map>
     </div>
   );
