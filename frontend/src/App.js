d7f0da4ca1f96269b2815a5a22c8c23bebaeac1b
JaehyeonPaak <wogus392@gmail.com>
2023-07-15 10:48:52 +0900

    feat: add react-map-gl package and set default map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import React, {useState} from 'react';
+import Map from 'react-map-gl';
+
 function App() {
+  const [viewState, setViewState] = useState({
+    longitude: -122.4,
+    latitude: 37.8,
+    zoom: 8
+  });
+
   return (
-    <div className="App">
-      Test
+    <div className='App'>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        initialViewState={viewState}
+        style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
     </div>
   );
 }
