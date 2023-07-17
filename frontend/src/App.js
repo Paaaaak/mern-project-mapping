b5ae568278b36fa880b515d556013dc60230a792
JaehyeonPaak <wogus392@gmail.com>
2023-07-17 21:47:36 +0900

    feat: toggle pop up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,21 @@ import Map, {Marker, Popup} from 'react-map-gl';
 import {Room, Star} from '@material-ui/icons'
 import './App.css';
 import axios from 'axios';
+import {format} from 'timeago.js';
 
 function App() {
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [showPopup, setShowPopup] = useState(true);
+  const [pins, setPins] = useState([]);
   const [viewState, setViewState] = useState({
     longitude: -122.4,
     latitude: 37.8,
-    zoom: 8
+    zoom: 5
   });
 
-  const [showPopup, setShowPopup] = useState(true);
-
-  const [pins, setPins] = useState([]);
+  const markerClickHandler = (pinId) => {
+    setCurrentPlaceId(pinId);
+  };
 
   // get all pins from database everytime refreshing the page
   useEffect(() => {
@@ -37,37 +41,42 @@ function App() {
         style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}
         mapStyle="mapbox://styles/jaehyeonpaak/clk3lonwv000q01rd0jcu3lsf">
         {pins.map((pin) => (
-          <React.Fragment>
+          <div>
             <Marker
               longitude={pin.long}
               latitude={pin.lat}
               anchor='bottom'>
-              <Room style={{ fontSize: viewState.zoom * 6, color: 'red' }}></Room>
+              <Room 
+                style={{ fontSize: viewState.zoom * 6, color: 'red' }}
+                onClick={() => markerClickHandler(pin._id)}>  
+              </Room>
             </Marker>
-            <Popup
-              longitude={pin.long}
-              latitude={pin.lat}
-              anchor="left"
-              onClose={() => setShowPopup(false)}>
-              <div className='card'>
-                <label>Place</label>
-                <h4 className='place'>{pin.title}</h4>
-                <label>Review</label>
-                <p className='description'>{pin.discription}</p>
-                <label>Rating</label>
-                <div className='stars'>
-                  <Star className='star'></Star>
-                  <Star className='star'></Star>
-                  <Star className='star'></Star>
-                  <Star className='star'></Star>
-                  <Star className='star'></Star>
+            {pin._id === currentPlaceId && (
+              <Popup
+                longitude={pin.long}
+                latitude={pin.lat}
+                anchor="left"
+                onClose={() => setShowPopup(false)}>
+                <div className='card'>
+                  <label>Place</label>
+                  <h4 className='place'>{pin.title}</h4>
+                  <label>Review</label>
+                  <p className='description'>{pin.discription}</p>
+                  <label>Rating</label>
+                  <div className='stars'>
+                    <Star className='star'></Star>
+                    <Star className='star'></Star>
+                    <Star className='star'></Star>
+                    <Star className='star'></Star>
+                    <Star className='star'></Star>
+                  </div>
+                  <label>Information</label>
+                  <span className='username'>Created by <b>{pin.username}</b></span>
+                  <span className='date'>{format(pin.createdAt)}</span>
                 </div>
-                <label>Information</label>
-                <span className='username'>Created by <b>{pin.username}</b></span>
-                <span className='date'>1 hour ago</span>
-              </div>
-            </Popup>
-          </React.Fragment>
+              </Popup>
+            )}
+          </div>
         ))}
       </Map>
     </div>
