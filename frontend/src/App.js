9cdf1d5ae3285d6d9be7c5b9e12ff607dc817baa
JaehyeonPaak <wogus392@gmail.com>
2023-07-17 19:59:52 +0900

    feat: down grade react version and get all pins from database by using usereffect hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Map, {Marker, Popup} from 'react-map-gl';
 import {Room, Star} from '@material-ui/icons'
 import './App.css';
+import axios from 'axios';
 
 function App() {
   const [viewState, setViewState] = useState({
@@ -12,6 +13,22 @@ function App() {
 
   const [showPopup, setShowPopup] = useState(true);
 
+  const [pins, setPins] = useState([]);
+
+  // get all pins from database everytime refreshing the page
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get('/pins');
+        setPins(res.data);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+    getPins();
+  }, []);
+
   return (
     <div className='App'>
       <Map
@@ -19,35 +36,39 @@ function App() {
         initialViewState={viewState}
         style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}
         mapStyle="mapbox://styles/jaehyeonpaak/clk3lonwv000q01rd0jcu3lsf">
-        <Marker 
-          longitude={-122.4}
-          latitude={37.8}
-          anchor='bottom'>
-          <Room style={{fontSize: viewState.zoom * 6, color: 'red'}}></Room>
-        </Marker>
-        {/* <Popup 
-          longitude={-122.4} 
-          latitude={37.8}
-          anchor="left"
-          onClose={() => setShowPopup(false)}>
-          <div className='card'>
-            <label>Place</label>
-            <h4 className='place'>Lotte Tower</h4>
-            <label>Review</label>
-            <p className='description'>Fascinating</p>
-            <label>Rating</label>
-            <div className='stars'>
-              <Star className='star'></Star>
-              <Star className='star'></Star>
-              <Star className='star'></Star>
-              <Star className='star'></Star>
-              <Star className='star'></Star>
-            </div>
-            <label>Information</label>
-            <span className='username'>Created by <b>jaehyeonpaak</b></span>
-            <span className='date'>1 hour ago</span>
-          </div>
-        </Popup> */}
+        {pins.map((pin) => (
+          <React.Fragment>
+            <Marker
+              longitude={pin.long}
+              latitude={pin.lat}
+              anchor='bottom'>
+              <Room style={{ fontSize: viewState.zoom * 6, color: 'red' }}></Room>
+            </Marker>
+            <Popup
+              longitude={pin.long}
+              latitude={pin.lat}
+              anchor="left"
+              onClose={() => setShowPopup(false)}>
+              <div className='card'>
+                <label>Place</label>
+                <h4 className='place'>{pin.title}</h4>
+                <label>Review</label>
+                <p className='description'>{pin.discription}</p>
+                <label>Rating</label>
+                <div className='stars'>
+                  <Star className='star'></Star>
+                  <Star className='star'></Star>
+                  <Star className='star'></Star>
+                  <Star className='star'></Star>
+                  <Star className='star'></Star>
+                </div>
+                <label>Information</label>
+                <span className='username'>Created by <b>{pin.username}</b></span>
+                <span className='date'>1 hour ago</span>
+              </div>
+            </Popup>
+          </React.Fragment>
+        ))}
       </Map>
     </div>
   );
