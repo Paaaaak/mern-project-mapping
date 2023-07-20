d7c052b8624d20a5c2e003b5ce12e643811a6e9f
JaehyeonPaak <wogus392@gmail.com>
2023-07-20 23:56:21 +0900

    feat: finish register form

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import './Register.css';
 import Footprint from '../assets/footprint.png';
+import axios from 'axios';
 
 const Register = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const newUser = {
+      username: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    };
+
+    try {
+      const res = await axios.post('/users/register', newUser);
+      console.log(res.data);
+      setSuccess(true);
+      setError(false);
+    }
+    catch (error) {
+      console.log(error);
+      setSuccess(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className='register-container'>
-        <div className='register-title'>
-            <img src={Footprint}></img>
-            <span>Footprint</span>
+      <div className='register-title'>
+        <img src={Footprint}></img>
+        <span>Footprint</span>
+      </div>
+      <form onSubmit={submitHandler}>
+        <input type='text' placeholder='User name' ref={nameRef}></input>
+        <input type='email' placeholder='Email' ref={emailRef}></input>
+        <input type='password' placeholder='Password' ref={passwordRef}></input>
+        <button className='register-button'>Register</button>
+        <div className='register-result'>
+          {success && <span className='success'>Successfull. You can login now!</span>}
+          {error && <span className='failure'>Something went wrong! Contact to administrator</span>}
         </div>
-        <form>
-            <input type='text' placeholder='User name'></input>
-            <input type='email' placeholder='Email'></input>
-            <input type='password' placeholder='Password'></input>
-            <button>Register</button>
-        </form>
+      </form>
     </div>
   );
 }
