296dcc8fa79c5529e64666e3b31b35cd30f76f5c
JaehyeonPaak <wogus392@gmail.com>
2023-07-24 17:35:45 +0900

    feat: add backdrop of register page

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -3,6 +3,7 @@ import './Register.css';
 import Footprint from '../assets/footprint.png';
 import axios from 'axios';
 import {Cancel} from '@material-ui/icons'
+import ReactDom from 'react-dom';
 
 const Register = (props) => {
   const [success, setSuccess] = useState(false);
@@ -42,23 +43,32 @@ const Register = (props) => {
   };
 
   return (
-    <div className='register-container'>
-      <div className='register-title'>
-        <img src={Footprint}></img>
-        <span>Footprint</span>
-      </div>
-      <form onSubmit={submitHandler}>
-        <input type='text' placeholder='User name' ref={nameRef}></input>
-        <input type='email' placeholder='Email' ref={emailRef}></input>
-        <input type='password' placeholder='Password' minLength={6} ref={passwordRef}></input>
-        <button className='register-button'>Register</button>
-        <div className='register-result'>
-          {success && <span className='success'>Successfull. You can login now!</span>}
-          {error && <span className='failure'>Something went wrong! Contact to administrator</span>}
-        </div>
-      </form>
-      <Cancel className='register-cancel' onClick={cancelClickHandler}></Cancel>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <div className='backdrop'></div>,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDom.createPortal(
+        <div className='register-container'>
+          <div className='register-title'>
+            <img src={Footprint}></img>
+            <span>Footprint</span>
+          </div>
+          <form onSubmit={submitHandler}>
+            <input type='text' placeholder='User name' ref={nameRef}></input>
+            <input type='email' placeholder='Email' ref={emailRef}></input>
+            <input type='password' placeholder='Password' minLength={6} ref={passwordRef}></input>
+            <button className='register-button'>Register</button>
+            <div className='register-result'>
+              {success && <span className='success'>Successfull. You can login now!</span>}
+              {error && <span className='failure'>Something went wrong! Contact to administrator</span>}
+            </div>
+          </form>
+          <Cancel className='register-cancel' onClick={cancelClickHandler}></Cancel>
+        </div>,
+        document.getElementById('overlay-root')
+      )}
+    </React.Fragment>
   );
 }
 
