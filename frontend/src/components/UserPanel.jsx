84ef163b0787803b399f86ce1f7edf2fa9d4f31b
JaehyeonPaak <wogus392@gmail.com>
2023-07-26 18:22:01 +0900

    feat: add useeffect hook to manage color

--- frontend/src/components/UserPanel.jsx
+++ frontend/src/components/UserPanel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './UserPanel.css';
 import { useState } from "react";
 import { motion } from "framer-motion";
@@ -18,6 +18,19 @@ const UserPanel = (props) => {
   };
 
   const [showUser, setShowUser] = useState(false);
+  const [color, setColor] = useState(null);
+
+  useEffect(() => {
+    // 0.5초 뒤에 실행
+    const identifier = setTimeout(() => {
+      console.log(color);
+      // 색상 업데이트 axios 호출문 추가
+    }, 500);
+    
+    return () => {
+      clearTimeout(identifier);
+    }
+  }, [color]);
 
   return (
     <div className='user-panel'>
@@ -33,7 +46,7 @@ const UserPanel = (props) => {
           <span>Welcome <b>{props.currentUser}</b>!</span>
           <div className='info-color'>
             <span>Current pin color: </span>
-            <input type='color'></input>
+            <input type='color' onChange={(event) => setColor(event.target.value)}></input>
           </div>
           <motion.button 
             whileHover={{ scale: 1.1 }}
