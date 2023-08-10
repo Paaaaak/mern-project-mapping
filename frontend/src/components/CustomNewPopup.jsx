36b397452ffa2e70f97d2917f0c8f19ddbf4a03e
JaehyeonPaak <wogus392@gmail.com>
2023-08-11 00:16:16 +0900

    style: refactor register page

--- frontend/src/components/CustomNewPopup.jsx
+++ frontend/src/components/CustomNewPopup.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import './CustomNewPopup.css';
-import {Popup} from 'react-map-gl';
+import { Popup } from 'react-map-gl';
 
 const CustomNewPopup = (props) => {
   return (
     <Popup
-    longitude={props.newPlace.longitude}
-    latitude={props.newPlace.latitude}
-    anchor="left"
-    onClose={() => props.setNewPlace(null)}>
-    <div className='new-card'>
-      <form onSubmit={props.submitHandler} className='popup-form'>
-        <label>Title</label>
-        <input placeholder='Enter a title' onChange={(event) => props.setTitle(event.target.value)}></input>
-        <label>Review</label>
-        <textarea placeholder='Leave your impression about this place!' onChange={(event) => props.setDescription(event.target.value)}></textarea>
-        <label>Rating</label>
-        <select onChange={(event) => props.setRating(event.target.value)}>
-          <option defaultValue='1'>1</option>
-          <option value='2'>2</option>
-          <option value='3'>3</option>
-          <option value='4'>4</option>
-          <option value='5'>5</option>
-        </select>
-        <button className='submit-button' type='submit'>Add Pin</button>
-      </form>
-    </div>
-  </Popup>
+      longitude={props.newPlace.longitude}
+      latitude={props.newPlace.latitude}
+      anchor="left"
+      onClose={() => props.setNewPlace(null)}>
+      <div className='new-card'>
+        <form onSubmit={props.submitHandler} className='popup-form'>
+          <label>Title</label>
+          <input placeholder='Enter a title' onChange={(event) => props.setTitle(event.target.value)}></input>
+          <label>Review</label>
+          <textarea placeholder='Leave your impression about this place!' onChange={(event) => props.setDescription(event.target.value)}></textarea>
+          <label>Rating</label>
+          <select onChange={(event) => props.setRating(event.target.value)}>
+            <option defaultValue='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+          </select>
+          <button className='submit-button' type='submit'>Add Pin</button>
+        </form>
+      </div>
+    </Popup>
   );
 }
 
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './Register.css';
 import Footprint from '../assets/footprint.png';
 import axios from 'axios';
@@ -10,6 +10,7 @@ import LockOpenIcon from '@mui/icons-material/LockOpen';
 const Register = (props) => {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const nameRef = useRef();
   const emailRef = useRef();
@@ -23,8 +24,6 @@ const Register = (props) => {
       password: passwordRef.current.value
     };
 
-    console.log(newUser);
-
     try {
       const res = await axios.post('/users/register', newUser);
       console.log(res.data);
@@ -36,6 +35,7 @@ const Register = (props) => {
       console.log('Error message: ' + error.response.data);
       setSuccess(false);
       setError(true);
+      setErrorMessage('Something went wrong! Contact to administrator');
     }
   };
 
@@ -45,6 +45,24 @@ const Register = (props) => {
     setError(false);
     console.log('Cancel clicked!');
   };
+
+  useEffect(() => {
+    if (success) {
+      // 성공 이벤트 렌더링 및 로그인 페이지로 리다이렉트
+    }
+  }, [success]);
+
+  // Username 입력 시 특수문자 및 대문자를 포함하면 경고
+  // Email 입력 시 Email Form과 일치하지 않으면 경고
+  // Password 입력하지 않고 Confirm Password부터 입력 시 경고 및 Password로 Focus
+  // Register 버튼 클릭 시 Username이 중복되는지 체크
+
+  // Register 성공됐을 경우 Login 페이지로 Redirect
+
+  // Username can only contain lowercase alphabets and numbers
+  // Please enter the password first 
+  // Write the correct email form including @
+  // Username is duplicated 
 
   return (
     <React.Fragment>
@@ -56,7 +74,6 @@ const Register = (props) => {
           </div>
           <h1>Register</h1>
           <form className='register-form' onSubmit={submitHandler}>
-            {/* <input type='text' placeholder='User name' ref={nameRef}></input> */}
             <div>
               <label for='username'>Username</label>
               <div className='input-container'>
@@ -64,7 +81,6 @@ const Register = (props) => {
                 <input id='username' type='text' placeholder='User name' ref={nameRef}></input>
               </div>
             </div>
-            <p className='register-warning'>※ Username can only contain lowercase alphabets and numbers</p>
             <div>
               <label for='email'>Email</label>
               <div className='input-container'>
@@ -72,8 +88,6 @@ const Register = (props) => {
                 <input type='email' placeholder='Email' ref={emailRef}></input>
               </div>
             </div>
-            {/* <input type='password' placeholder='Password' minLength={6} ref={passwordRef}></input>
-            <input type='password' placeholder='Confirm password' minLength={6}></input> */}
             <div>
               <label for='password'>Password</label>
               <div className='input-container'>
@@ -92,7 +106,7 @@ const Register = (props) => {
             <button className='register-cancel' onClick={cancelClickHandler}>Cancel</button>
             <div className='register-result'>
               {success && <span className='success'>Successfull. You can login now!</span>}
-              {error && <span className='failure'>Something went wrong! Contact to administrator</span>}
+              {error && <span className='failure'>{errorMessage}</span>}
             </div>
           </form>
         </div>,
