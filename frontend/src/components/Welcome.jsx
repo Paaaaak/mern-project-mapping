c915d100afc9665f2c672d56b53ec489e7703979
JaehyeonPaak <wogus392@gmail.com>
2023-09-04 11:49:08 +0900

    style: sort welcome banner

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -6,15 +6,15 @@ const Welcome = (props) => {
   return (
     <div className={props.showWelcome ? 'welcome-container' : 'welcome-hidden-container'}>
       <div className='welcome-logo-container'>
-        <img src={FootprintAnimation} alt='animation' style={{transform: 'scale(0.3)', marginLeft: '300px'}}></img>
-        <span>Welcome {props.currentUser}!</span>
+        <div>
+          <img src={FootprintAnimation} className='welcome-logo' alt='animation' style={{transform: 'scale(0.25)'}}></img>
+          <span>Welcome {props.currentUser}!</span>
+        </div>
       </div>
       <div className="firework"></div>
       <div className="firework"></div>
       <div className="firework"></div>
       <div className="firework"></div>
-      {/* <div className="firework"></div>
-      <div className="firework"></div> */}
     </div>
   );
 };
